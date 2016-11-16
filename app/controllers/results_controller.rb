90ae36cd64cd830bc0c3f0bdbb297daa851450af
justmartin <justin.lamar.martin@gmail.com>
2016-11-15 21:47:32 -0800

    Merged cost and category hash to obtain category name.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,15 @@
 class ResultsController < ApplicationController
 
   def index
-    @parsed = BudgetYourTrip.get_country_average(params[:country])
+
+    @costs = BudgetYourTrip.get_country_average(params[:country])
+    @categories = BudgetYourTrip.get_cost_categories
+
+    @categories["data"].each do |name|
+      @costs["data"].each do |id|
+        id.merge!(name) if id["category_id"] == name["category_id"]
+      end
+    end
   end
   
 end
--- app/models/budget_your_trip.rb
+++ app/models/budget_your_trip.rb
@@ -11,4 +11,17 @@ class BudgetYourTrip
 
     JSON.parse(res.body).to_h
   end
+
+  def self.get_cost_categories
+    url = URI.parse("http://www.budgetyourtrip.com/api/v3/categories")
+
+    req = Net::HTTP::Get.new(url.path)
+    req.add_field("X-API-KEY", "justin.lamar.martin@gmail.com")
+
+    res = Net::HTTP.new(url.host, url.port).start do |http|
+      http.request(req)
+    end
+
+    JSON.parse(res.body).to_h
+  end
 end
