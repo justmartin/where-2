97ad5b64419981acb3ed9ecc6b4325aa74de9282
justmartin <justin.lamar.martin@gmail.com>
2016-11-16 12:35:14 -0800

    Added currency_code to costs hash.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,14 +1,19 @@
 class ResultsController < ApplicationController
 
   def index
-    @costs = BudgetYourTrip.get_country_average(params[:country])
-    @categories = BudgetYourTrip.get_cost_categories
+    @costs = BudgetYourTrip.get_country_average_costs(params[:country])["data"]
+    @categories = BudgetYourTrip.get_cost_categories["data"]
+    @code = BudgetYourTrip.get_currency_code(params[:country])
+    # @budget = BudgetYourTrip.days_on_budget(params[:budget], @costs["data"][-1]["currency_code"], @costs["data"][-1]["value_budget"])
 
-    @categories["data"].each do |name|
-      @costs["data"].each do |id|
+    @categories.each do |name|
+      @costs.each do |id|
         id.merge!(name) if id["category_id"] == name["category_id"]
       end
     end
+
+    @costs.each { |cost| cost.merge!(@code)}
+    
   end
   
 end
--- app/models/budget_your_trip.rb
+++ app/models/budget_your_trip.rb
@@ -1,20 +1,38 @@
 class BudgetYourTrip
-  def self.get_country_average(country)
+  def self.get_country_average_costs(country)
     url = URI.parse("http://www.budgetyourtrip.com/api/v3/costs/country/" + country)
 
-    req = Net::HTTP::Get.new(url.path)
-    req.add_field("X-API-KEY", "justin.lamar.martin@gmail.com")
-
-    res = Net::HTTP.new(url.host, url.port).start do |http|
-      http.request(req)
-    end
-
-    JSON.parse(res.body).to_h
+    api_call(url)
   end
 
   def self.get_cost_categories
     url = URI.parse("http://www.budgetyourtrip.com/api/v3/categories")
 
+    api_call(url)
+  end
+
+  def self.get_currency_code(country_code)
+    url = URI.parse("http://www.budgetyourtrip.com/api/v3/countries/" + country_code)
+
+    api_call(url)
+  end
+
+
+  # def self.days_on_budget(budget, currency_code, country_budget)
+  #   country_budget_usd = convert_to_usd(currency_code, country_budget)
+
+  #   budget.to_i / country_budget_usd["newAmount"]
+  # end
+
+  # def self.convert_to_usd(currency_code, amount)
+  #   url = URI.parse("http://www.budgetyourtrip.com/api/v3/currencies/convert/" + currency_code + "/usd/"+ amount)
+
+  #   api_call(url)
+  # end
+
+  private
+
+  def self.api_call(url)
     req = Net::HTTP::Get.new(url.path)
     req.add_field("X-API-KEY", "justin.lamar.martin@gmail.com")
 
@@ -24,4 +42,6 @@ class BudgetYourTrip
 
     JSON.parse(res.body).to_h
   end
+
+
 end
