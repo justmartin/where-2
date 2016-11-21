b1b0ce9f9aa7dae343a23aa0cae82222df9ff733
justmartin <justin.lamar.martin@gmail.com>
2016-11-20 19:06:00 -0800

    Switched default currency on results page to be USD.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,7 +1,6 @@
 class ResultsController < ApplicationController
 
   def index
-
     @costs = BudgetYourTrip.get_country_average_costs(params[:country])["data"]
     @categories = BudgetYourTrip.get_cost_categories["data"]
     @code = BudgetYourTrip.get_currency_code(params[:country])
@@ -23,7 +22,6 @@ class ResultsController < ApplicationController
     @budget = BudgetYourTrip.days_on_budget(params[:budget], @costs[-1]["currency_code"], @costs[-1]["value_budget"])
     @midrange = BudgetYourTrip.days_on_budget(params[:budget], @costs[-1]["currency_code"], @costs[-1]["value_midrange"])
     @luxury = BudgetYourTrip.days_on_budget(params[:budget], @costs[-1]["currency_code"], @costs[-1]["value_luxury"])
-
   end
   
 end
