e00befbe26bfd9f24da2300c85ffdb3f0b8adfe7
justmartin <justin.lamar.martin@gmail.com>
2016-11-21 16:42:51 -0800

    Added rspec test cases for convert_to_usd and get_country_average_costs methods.

--- spec/models/budget_your_trip_spec.rb
+++ spec/models/budget_your_trip_spec.rb
@@ -5,28 +5,56 @@ describe BudgetYourTrip do
   describe ".days_on_budget" do
     it "calculates number of days for budget/midrange/luxury" do
       budget = "1000"
-      country_code = "THB"
+      currency_code = "THB"
       budget_amount = "660.84723802611"
+
       expect(BudgetYourTrip.days_on_budget("1000", "THB", "660.84723802611")).to eq 55
     end
 
     it "calculates number of days for budget/midrange/luxury" do
       budget = "1000"
-      country_code = "THB"
+      currency_code = "THB"
       midrange_amount = "1804.722688247"
+
       expect(BudgetYourTrip.days_on_budget("1000", "THB", "1804.722688247")).to eq 20
     end
 
     it "calculates number of days for budget/midrange/luxury" do
       budget = "1000"
-      country_code = "THB"
+      currency_code = "THB"
       luxury_amount = "5323.9282342373"
+
       expect(BudgetYourTrip.days_on_budget("1000", "THB", "5323.9282342373")).to eq 6
     end
   end
 
-  
+  describe ".convert_to_usd" do
+    it "converts currency amounts to usd" do
+      currency_code = "THB"
+      amount = "5323.9282342373"
 
+      expect(BudgetYourTrip.convert_to_usd(currency_code, amount)["data"]["newAmount"]).to eq 149.96980941513
+    end
+  end
 
-  
+  describe ".get_country_average_costs" do
+    it "retrieves daily average for budget/midrange/luxury" do
+      country = "TH"
+
+      expect(BudgetYourTrip.get_country_average_costs(country)["data"][-1]["value_budget"]).to eq "660.84723802611"
+    end
+
+    it "retrieves daily average for budget/midrange/luxury" do
+      country = "TH"
+
+      expect(BudgetYourTrip.get_country_average_costs(country)["data"][-1]["value_midrange"]).to eq "1804.722688247" 
+    end
+
+    it "retrieves daily average for budget/midrange/luxury" do
+      country = "TH"
+
+      expect(BudgetYourTrip.get_country_average_costs(country)["data"][-1]["value_luxury"]).to eq "5323.9282342373"
+    end
+  end
+
 end
